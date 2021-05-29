1fc127ca8e22acc25e2c7cae19a4028481c376fb
Cherrie Awang <caflores31@gmail.com>
2021-05-28 19:31:43 -0700

    searchbooks

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -9,24 +9,22 @@ import { SAVE_BOOK } from '../utils/mutations';
 import {useMutation} from '@apollo/react-hooks';
 
 const SearchBooks = () => {
-  // create state for holding returned google api data
+  //  state for holding returned google api data //
   const [searchedBooks, setSearchedBooks] = useState([]);
-  // create state for holding our search field data
+  //  state for holding our search field //
   const [searchInput, setSearchInput] = useState('');
 
-  // create state to hold saved bookId values
+  // hold saved bookId values //
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
   const [saveBook, {error}] = useMutation(SAVE_BOOK );
 
-
-  // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
-  // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
+  // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup //
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
   });
 
-  // create method to search for books and set state on form submit
+  //  method to search for books and set state on form submit //
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
@@ -58,7 +56,7 @@ const SearchBooks = () => {
     }
   };
 
-  // create function to handle saving a book to our database
+  // handle saving a book to our database //
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
